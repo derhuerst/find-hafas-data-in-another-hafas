8e7d3f53bb00497ebd0a7a303a1fa57949aeadd3
Jannis R <mail@jannisr.de>
2019-12-14 01:54:24 +0100

    WIP

--- index.js
+++ index.js
@@ -1,18 +1,55 @@
 'use strict'
 
-const slug = require('slug')
+const _distance = require('@turf/distance').default
+const {point} = require('@turf/helpers')
+const without = require('lodash/without')
+const tokenizeDb = require('tokenize-db-station-name')
+const tokenizeVbb = require('tokenize-vbb-station-name')
 const debug = require('debug')('find-db-hafas-trip-in-another-hafas')
 
 const minute = 60 * 1000
 const nonEmptyStr = str => 'string' === typeof str && str.length > 0
 
+const distance = (lA, lB) => {
+	return _distance(
+		point([lA.longitude, lA.latitude]),
+		point([lB.longitude, lB.latitude]),
+		{units: 'kilometers'}
+	)
+}
+
+// todo: use same `language`
+
+// todo: move to tokenize-db-station-name
+const dbStopwords = [
+	'bahnhof',
+	'berlin',
+	'sbahn',
+
+	'bayern', 'thueringen', 'sachsen', 'anhalt', 'westfalen', 'wuerttemberg', 'oberpfalz', 'schwaben', 'oberbayern', 'holstein', 'braunschweig', 'saalekreis', 'saalekreis', 'niederbayern', 'schwarzwald', 'oldenburg', 'uckermark', 'rheinland', 'oberfranken', 'rheinhessen', 'hessen', 'altmark', 'limesstadt', 'vogtland', 'mecklenburg', 'mittelfranken', 'dillkreis', 'odenwald', 'erzgebirge', 'prignitz', 'oberhessen', 'ostfriesland', 'schleswig', 'unterfranken', 'westerwald', 'dithmarschen',
+	// todo: 'saechsische schweiz', 'thueringer wald', 'schaumburg lippe', 'frankfurt main'
+	'bahnhof',
+	'fernbahnhof'
+]
+
+// todo: move to tokenize-vbb-station-name
+const vbbStopwords = [
+	'bahnhof',
+	'berlin', 'polen',
+	'sbahn', 'ubahn'
+]
+
+// todo: pass these in
+const normalizeNameA = str => without(tokenizeDb(str), ...dbStopwords).join(' ')
+const normalizeNameB = str => without(tokenizeVbb(str), ...vbbStopwords).join(' ')
+
 const matchStopOrStationByName = (stopA) => {
-	const stopAName = slug(stopA.name)
-	const stationAName = stopA.station ? slug(stopA.station.name) : NaN
+	const stopAName = normalizeNameA(stopA.name)
+	const stationAName = stopA.station ? normalizeNameA(stopA.station.name) : NaN
 	return (stopB) => {
-		const stopBName = slug(stopB.name)
+		const stopBName = normalizeNameB(stopB.name)
 		if (stopBName === stopAName) return true
-		const stationBName = stopB.station ? slug(stopB.station.name) : NaN
+		const stationBName = stopB.station ? normalizeNameB(stopB.station.name) : NaN
 		return (
 			stopBName === stationAName ||
 			stationBName === stopAName ||
@@ -22,21 +59,27 @@ const matchStopOrStationByName = (stopA) => {
 }
 
 const findStopByName = async (hafasB, stopA) => {
-	const stopAName = slug(stopA.name)
-	const match = stopB => slug(stopB.name) === stopAName
+	debug('findStopByName', stopA.id, stopA.name)
 
-	const fuzzy = await hafasB.locations(stopA.name, {
-		addresses: false, poi: false
-	})
-	return fuzzy.find(match) || fuzzy.find(matchStopOrStationByName(stopA)) || null
+	const nearby = await hafasB.nearby(stopA.location, {poi: false})
+	debug('hafasB.nearby()', stopA.location, nearby.map(loc => [loc.id, loc.name]))
+
+	const matchA = matchStopOrStationByName(stopA)
+	return nearby.find(matchA) || null
+
+	// todo
+	// const fuzzy = await hafasB.locations(stopA.name, {
+	// 	addresses: false, poi: false
+	// })
 }
 
 // todo: upgrade `hafas-client`
-const leadingZeros = /^0+/
-const findStopById = async (hafasB, idA) => {
+// const leadingZeros = /^0+/
+const findStopById = async (hafasB, stopA) => {
+	debug('findStopById', stopA.id, stopA.name)
 	try {
 		const exact = await hafasB.stop(stopA)
-		return exact
+		return distance(exact.location, stopA.location) < .2 ? exact : null
 	} catch (err) {
 		if (err && err.isHafasError) return null
 		throw err
@@ -44,48 +87,66 @@ const findStopById = async (hafasB, idA) => {
 }
 
 const findStop = async (hafasB, sA) => {
-	let sB = await findStopById(hafasB, sA.id)
+	debug('findStop', sA.id, sA.name)
+
+	let sB = await findStopById(hafasB, sA)
 	if (sB) {
-		debug(sA.id, sA.name, 'matched by stop ID with', sB.id, sB.name)
+		debug('matched by stop ID with', sB.id, sB.name)
 		return sB
 	}
 	if (sA.station) {
-		sB = await findStopById(hafasB, sA.station.id)
+		sB = await findStopById(hafasB, sA.station)
 		if (sB) {
-			debug(sA.id, sA.name, 'matched by station ID', sA.station.id)
+			debug('matched by station ID', sA.station.id)
 			return sB
 		}
 	}
 	sB = await findStopByName(hafasB, sA)
 	if (sB) {
-		debug(sA.id, sA.name, 'matched by name with', sB.id, sB.name)
+		debug('matched by name with', sB.id, sB.name)
 		return sB
 	}
+	if (sA.station) {
+		sB = await findStopByName(hafasB, sA.station)
+		if (sB) {
+			debug('matched by station name with', sB.id, sB.name)
+			return sB
+		}
+	}
+
+	debug('not matched :(')
 	return null
 }
+
+module.exports = {findStop}
 
 const scheduledDepartureOf = (stopover) => {
 	let dep = +new Date(stopover.departure)
 	if (stopover.cancelled) dep = +new Date(stopover.formerScheduledDeparture)
-	else if (stopover.departureDelay) dep -= stopover.departureDelay * 1000
+	else if (Number.isFinite(stopover.departureDelay)) {
+		dep -= stopover.departureDelay * 1000
+	}
 	return Number.isNaN(dep) ? null : dep
 }
 const scheduledArrivalOf = (stopover) => {
 	let arr = +new Date(stopover.arrival)
 	if (stopover.cancelled) arr = +new Date(stopover.formerScheduledArrival)
-	else if (stopover.arrivalDelay) arr -= stopover.arrivalDelay * 1000
+	else if (Number.isFinite(stopover.arrivalDelay)) {
+		arr -= stopover.arrivalDelay * 1000
+	}
 	return Number.isNaN(arr) ? null : arr
 }
 
-
+// todo: use stable-public-transport-ids
 const createFindLeg = (hafasA, hafasB) => {
 	const findTripInAnotherHafas = async (legA) => {
-		if (!nonEmptyStr(legA.id)) throw new Error('legA.id must be a trip ID.')
+		if (!nonEmptyStr(legA.tripId)) throw new Error('legA.tripId must be a trip ID.')
 		if (!legA.line) throw new Error('legA.line must be an object.')
 		if (!nonEmptyStr(legA.line.name)) {
 			throw new Error('legA.line.name must be a non-empty string.')
 		}
-		debug('legA', legA.id, legA.line.name)
+		if (legA.walking) throw new Error('legA must not be a walking leg.')
+		debug('legA', legA.tripId, legA.line.name)
 
 		if (!Array.isArray(legA.stopovers) || legA.stopovers.length === 0) {
 			throw new Error('legA.stopovers must be an array with >0 items.')
@@ -105,13 +166,16 @@ const createFindLeg = (hafasA, hafasB) => {
 
 		// try to pass the trip ID from HAFAS A into HAFAS B
 		if (hafasB.trip) {
-			const pTrip = hafasB.trip(legA.id, legA.line.name, {
+			const pTrip = hafasB.trip(legA.tripId, legA.line.name, {
 				stopovers: true, remarks: false
 				// todo: `language`?
 			})
 			try {
 				const tripB = await pTrip
 
+				if (!Array.isArray(tripB.stopovers)) {
+					throw new Error(`HAFAS B didn't return stopovers`)
+				}
 				const firstI = tripB.stopovers.findIndex((stB) => {
 					if (!matchStopOrStationByName(firstStopA)(stB)) return false
 					const depB = scheduledDepartureOf(stB)
@@ -126,11 +190,11 @@ const createFindLeg = (hafasA, hafasB) => {
 				if (lastI < 0) throw new Error('last stopover not matched')
 
 				// todo: fahrtNr, intermediate stopovers
-				console.error('match!')
+				debug('trip match!', 'tripA', tripA.id, 'tripB', tripB.id)
 				return null
 			} catch (err) {
 				if (err && err.isHafasError) {
-					debug('trip() matching failed:', err && err.message || (err + ''))
+					debug('trip matching failed:', err && err.message || (err + ''))
 				} else throw err
 			}
 		}
@@ -150,7 +214,29 @@ const createFindLeg = (hafasA, hafasB) => {
 		}
 		debug('lastStopB', lastStopB.id, lastStopB.name)
 
+		// Query departures at firstStopB in direction of lastStopB.
+		const deps = await hafasB.departures(firstStopB.id, {
+			when: depA - minute, duration: 2 * minute,
+			direction: lastStopB.id,
+			remarks: false
+		})
+
+		for (const dep of deps) {
+			if (dep.line.fahrtNr !== legA.line.fahrtNr) continue
+
+			try {
+				const trip = await hafasB.trip(dep.tripId, {stopovers: true})
+				return trip
+			} catch (err) {
+				const journeys = await hafas.journeys(firstStopB, lastStopB, {
+					when: depA, stopovers: true, tickets: false
+				})
+				// todo: find a matching journey
+			}
+		}
+
 		// todo
+		// debug('fahrtNr', legA.line.fahrtNr)
 		return null
 	}
 	return findTripInAnotherHafas
