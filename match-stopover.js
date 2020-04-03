1b2f2a875530fd427512493e5af5c57dae563612
Jannis R <mail@jannisr.de>
2020-04-03 19:45:33 +0200

    matchStopover: debug logging

--- match-stopover.js
+++ match-stopover.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const debug = require('debug')('find-hafas-data-in-another-hafas:match-stopover')
 const {plannedDepartureOf, plannedArrivalOf} = require('./lib/helpers')
 
 const createMatchStopover = (matchStopOrStation) => {
@@ -9,13 +10,18 @@ const createMatchStopover = (matchStopOrStation) => {
 		const arrA = plannedArrivalOf(stopoverA)
 
 		const matchStopover = (stopoverB) => {
-			if (!matchStopA(stopoverB.stop)) return false
+			if (!matchStopA(stopoverB.stop)) {
+				debug('matching stops failed', stopoverA.stop, stopoverB.stop)
+				return false
+			}
 			const depB = plannedDepartureOf(stopoverB)
 			const arrB = plannedArrivalOf(stopoverB)
-			return (
+			if (
 				(depB !== null && depB === depA) ||
 				(arrB !== null && arrB === arrA)
-			)
+			) return true
+			debug('matching date/time failed', arrA, depA, arrB, depB)
+			return false
 		}
 		return matchStopover
 	}
