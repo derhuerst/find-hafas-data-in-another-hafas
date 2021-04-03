dcf9fc001aa569da20026536706dab35660a1f82
Jannis R <mail@jannisr.de>
2021-04-03 20:20:15 +0200

    tokenize-vbb-station-name -> normalize-vbb-station-name-for-search
    Also bump tokenize-db-station-name to match some transformations introduced by normalize-vbb-station-name-for-search.

--- test/normalize-vbb-names.js
+++ test/normalize-vbb-names.js
@@ -1,30 +1,13 @@
 'use strict'
 
 const without = require('lodash/without')
-const tokenizeVbb = require('tokenize-vbb-station-name')
+const normalizeVbbStationName = require('normalize-vbb-station-name-for-search')
 const {strictEqual} = require('assert')
 const slug = require('slug')
 
-// todo: move to tokenize-vbb-station-name
-const vbbStopwords = [
-	'bahnhof',
-	'berlin', 'polen',
-	'sbahn', 'ubahn'
-]
-
-const normalizeStopName = (str) => {
-	return without(tokenizeVbb(str), ...vbbStopwords)
-	.reduce((tokens, token) => {
-		if (/[\w]strasse/g.test(token)) {
-			return [...tokens, token.slice(0, -7), token.slice(-7)]
-		}
-		return [...tokens, token]
-	}, [])
-	.join(' ')
+const normalizeStopName = (rawName) => {
+	return normalizeVbbStationName(rawName)
 }
-
-strictEqual(normalizeStopName('Foo Barstr.'), 'foo bar strasse')
-strictEqual(normalizeStopName('str.'), 'strasse')
 
 const normalizeLineName = str => slug(str.replace(/\s/g, ''))
 
